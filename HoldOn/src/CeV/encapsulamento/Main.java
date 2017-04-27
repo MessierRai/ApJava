f184183c4b4527bf75d03ce2a13ba03accb11319
Rai <rai.lima17@live.com>
2017-04-27 20:00:09 -0300

    2Quatro :)

---
+++ HoldOn/src/CeV/encapsulamento/Main.java
@@ -0,0 +1,15 @@
+package CeV.encapsulamento;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ControleRemoto c = new ControleRemoto();
+		
+		c.ligar();
+		c.maisVolume();
+		c.play();
+		c.abrirMenu();
+		c.fecharMenu();
+	}
+
+}
